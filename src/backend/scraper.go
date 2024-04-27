933d79ccc7763b1bf724612bc31af60fa9e813eb
AureliusJustin <88382259+AureliusJustin@users.noreply.github.com>
2024-04-27 23:25:01 +0700

    rapihin scraper.go

--- src/backend/scraper.go
+++ src/backend/scraper.go
@@ -14,18 +14,6 @@ type WikiPage struct {
 	URL   string
 }
 
-var user_agent = []string{
-	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.5 Safari/605.1.15",
-	"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36",
-	"Mozilla/5.0 (Windows NT 10.0; Windows; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36",
-	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8",
-	"Mozilla/5.0 (Windows NT 10.0; Windows; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36",
-	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15",
-	"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36",
-	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15",
-	"Mozilla/5.0 (Windows NT 10.0; Windows; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36",
-	"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36"}
-
 func getWikiLinks(page WikiPage) ([]WikiPage, error) {
 	visited2 := make(map[string]bool)
 	c := colly.NewCollector(
@@ -44,7 +32,7 @@ func getWikiLinks(page WikiPage) ([]WikiPage, error) {
 		tmp := e.Attr("href")
 		// fmt.Println(tmp)
 		// time.Sleep(time.Second)
-		if strings.HasPrefix(tmp, "/wiki") && !strings.HasPrefix(tmp, "/wiki/File:") && !strings.HasPrefix(tmp, "#") && !strings.HasPrefix(tmp, "https://") && !strings.HasPrefix(tmp, "/wiki/Special:") && !strings.HasPrefix(tmp, "/wiki/Category:") &&  !strings.HasPrefix(tmp, "/wiki/Wikipedia:") &&  !strings.HasPrefix(tmp, "/wiki/Portal:") &&  !strings.HasPrefix(tmp, "/wiki/Help:"){
+		if strings.HasPrefix(tmp, "/wiki") && !strings.HasPrefix(tmp, "/wiki/File:") && !strings.HasPrefix(tmp, "#") && !strings.HasPrefix(tmp, "https://") && !strings.HasPrefix(tmp, "/wiki/Special:") && !strings.HasPrefix(tmp, "/wiki/Category:") && !strings.HasPrefix(tmp, "/wiki/Wikipedia:") && !strings.HasPrefix(tmp, "/wiki/Portal:") && !strings.HasPrefix(tmp, "/wiki/Help:") {
 			wikipage.URL = "https://en.wikipedia.org" + tmp
 			// fmt.Println(wikipage.URL)
 			wikipage.Title = strings.TrimPrefix(wikipage.URL, "https://en.wikipedia.org/wiki/")
